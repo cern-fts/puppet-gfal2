6fa844db85fa79c218dd468334510cc251966775
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2017-06-07 14:18:15 +0200

    Add SIGNALS to mock plugin (DMC-959)

--- code/manifests/plugins/mock/params.pp
+++ code/manifests/plugins/mock/params.pp
@@ -1,5 +1,6 @@
 class gfal2::plugins::mock::params {
   $max_transfer_time = hiera('gfal2::plugins::mock::params::max_transfer_time', 5)
   $min_transfer_time = hiera('gfal2::plugins::mock::params::min_transfer_time', 5)
+  $signals = hiera('gfal2::plugins::mock::signals', 0)
 }
 
